2f3afba2796abc0347e2f82665df8cf6c6901048
Sam Goldberg <sgoldber61@gmail.com>
2017-12-15 13:17:00 -0800

    first attempt

---
+++ app.js
@@ -0,0 +1,27 @@
+// require non-express helper modules
+var path = require('path');
+var ipware = require('ipware');
+var requestIp = require('request-ip');
+
+// require express
+var express = require('express');
+var app = express();
+
+// app routing
+
+// render the home page index.html
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// example: {ipaddress: "72.198.91.159", language: "en-US", software: "Macintosh; Intel Mac OS X 10_11_1"}
+app.get('/whoami', function(req, res) {
+  var ipaddress = req.connection.remoteAddress;
+  
+  res.send({ipaddress: ipaddress, language: null, software: null});
+});
+
+// listen to port
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
