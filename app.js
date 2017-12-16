c46d8d7e70765bf5603c35b1457441f197f98ac6
Sam Goldberg <sgoldber61@gmail.com>
2017-12-15 18:47:06 -0800

    editing...

--- app.js
+++ app.js
@@ -16,14 +16,9 @@ app.get('/', function(req, res) {
 
 // example: {ipaddress: "72.198.91.159", language: "en-US", software: "Macintosh; Intel Mac OS X 10_11_1"}
 app.get('/whoami', function(req, res) {
-  var ipaddress = req.connection.remoteAddress;
+  var ipaddress = req.headers['x-forwarded-for'].split(",")[0];
   
-  var iparray = [req.headers['x-forwarded-for'],
-     req.connection.remoteAddress,
-     req.socket.remoteAddress,
-     (req.connection.socket ? req.connection.socket.remoteAddress : null)];
-  
-  res.send({ipaddress: ipaddress, ip0: iparray[0], ip1: iparray[1], ip2: iparray[2], ip3: iparray[3]});
+  res.send({ipaddress: ipaddress});
   
 });
 
